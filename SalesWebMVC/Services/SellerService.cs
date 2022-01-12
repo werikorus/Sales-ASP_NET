d3f85c5f42e4db61c8096a479f7806218232217a
Werik Santos <werik.msti@gmail.com>
2022-01-11 21:33:58 -0300

    Foreign Key not null (referential integrity)

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -23,10 +23,9 @@ namespace SalesWebMVC.Services
 
         public void Insert (Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
-
-
     }
 }
